ca1e8b4156f05a6790584d125f4f1b267cd85b37
0x4E43 <nimaic.dev@gmail.com>
2023-11-24 00:17:11 +0530

    User Input checks

--- main.go
+++ main.go
@@ -1,61 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
-
-// func main() {
-// 	println("Hello World")
-// 	//Read File
-// 	filePath := "main.yaml"
-// 	data, err := os.ReadFile(filePath)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	println(string(data))
-
-// 	//need to read indentation PHASE-1
-// 	//Lets print each bytr
-
-// 	println("TYPE : %T", data)
-
-// 	//print key
-// 	var keyList []string
-// 	var key string
-// 	for i := 0; i < len(data); i++ {
-
-// 		if string(data[i]) == ":" {
-// 			println("key: %v", key)
-// 			keyList = append(keyList, key)
-// 			println("ADDED")
-// 			key = ""
-// 		}
-// 		if string(data[i]) != "\n" && string(data[i]) != " " {
-// 			key += string(data[i])
-// 			println(key)
-
-// 		}
-
-// 	}
-
-// 	for j := 0; j < len(keyList); j++ {
-// 		println("KEY: %v", keyList[j])
-// 	}
-
-// 	//Example file writing
-// 	writeFilePath := "hello.txt"
-
-// 	writeData := []byte("Hello World")
-// 	fs, err := os.OpenFile(writeFilePath, os.O_APPEND, os.ModeAppend)
-// 	flag, err := fs.Write(writeData)
-// 	println(flag)
-// 	// err = os.WriteFile(writeFilePath, writeData, os.ModeAppend) //TODO: Append is not working
-// 	checkError(err)
-// 	fs.Close()
-
-// }
 
 func main() {
 	testConsole()
@@ -63,21 +11,44 @@ func main() {
 func checkError(err error) {
 	if err != nil {
 		panic(err)
+		//exit system
 	}
 
 }
-
 func testConsole() {
-	var value string
-	// fmt.Println("Hello World")
-	// fmt.Println("Enter Any thing")
-	// fmt.Scanf("%s", &value)
-	// fmt.Println("You have entered : ", value)
-	fmt.Print("joker # ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	value = scanner.Text()
+	ini := "joker # "
 
-	fmt.Println("Value: ", value)
+	fmt.Println("Welcome to the Joker DB 🃏 \n\n ")
+	showOptions()
+	for { //infinite loop
+		var choice int
+		fmt.Scan(&choice)
+		fmt.Println(ini, "Choice: ", choice)
+		if choice == 4 {
+			fmt.Println("Exiting System...")
+			os.Exit(1)
+		}
 
+		switch choice {
+		case 1:
+			fmt.Println(ini, "Creating DB")
+			break
+		case 2:
+			fmt.Println(ini, "Reading DB")
+			break
+		case 3:
+			fmt.Println(ini, "Deleting DB")
+			break
+		}
+		fmt.Println()
+		fmt.Print("\n", ini, "Waiting for user input: ")
+	}
+}
+
+func showOptions() {
+	fmt.Println("1. Create DB")
+	fmt.Println("2. Read DB")
+	fmt.Println("3. Delete DB")
+	fmt.Println("\nPress Esc to exit")
+	fmt.Println()
 }
