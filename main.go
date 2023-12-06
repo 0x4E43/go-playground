3435c4f15803623f695a56078c6b08b1a4a3889c
0x4E43 <nimaic.dev@gmail.com>
2023-12-06 19:20:29 +0530

    Pointer test

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func main() {
+func mai1n() {
 	// testConsole()
 	testArraySlice()
 }
---
+++ pointer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func zeroPointer(val int) {
+	val = 1
+}
+
+func pointer(val *int) {
+	val2 := 34
+	// *val = 0
+	val = &val2 //It only changes the local copy of the pointer
+	fmt.Println(val2, *val)
+}
+func main() {
+	val := 25
+	fmt.Println("Hello Pointer", val)
+	zeroPointer(val)
+	fmt.Println("Hello Pointer after calling by value", val)
+	pointer(&val)
+	fmt.Println("Hello Pointer after calling by ref", val, " Address: ", &val)
+
+}
