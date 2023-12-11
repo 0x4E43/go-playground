8268b939e854b2b54d203ca4f7dc36e95ad53115
0x4E43 <nimaic.dev@gmail.com>
2023-12-11 21:22:37 +0530

    still wondering why pointer

--- struct.go
+++ struct.go
@@ -10,7 +10,7 @@ type Nimai struct {
 func main() {
 	fmt.Println("Hello Struct test")
 
-	nimai := Nimai{"Nimai", 24}
+	// nimai := Nimai{"Nimai", 24}
 
 	var test []Nimai
 	test = append(test, Nimai{"Nimai", 24})
@@ -18,8 +18,10 @@ func main() {
 	test = append(test, Nimai{"Maikap", 24})
 
 	fmt.Println("Struct List length", len(test))
-	printStruct(nimai)
-	printStructSlice(test)
+	// printStruct(nimai)
+	// printStructSlice(test)
+
+	fmt.Println(CreatePerson().name)
 }
 
 func printStruct(nimai Nimai) {
@@ -32,4 +34,15 @@ func printStructSlice(structList []Nimai) {
 		fmt.Println("Name: ", structList[i].name, " Age: ", structList[i].age)
 	}
 
+}
+
+type Person struct {
+	name   string
+	age    int
+	mobile string
+}
+
+func CreatePerson() Person {
+	per := Person{"Nimai Charan Maikap", 24, ""}
+	return per
 }
