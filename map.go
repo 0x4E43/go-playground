7d2447e9272994a22cdc085c5e37072d69974283
0x4E43 <nimaic.dev@gmail.com>
2023-12-12 21:19:56 +0530

    map test

--- map.go
+++ map.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Data struct {
+	mData map[string]string
+}
 
 func main() {
-
 	fmt.Println("hello Map")
+	md := Data{make(map[string]string)}
+	md.SET("Hello", "World")
+
+	fmt.Println(md.mData["Hello"])
+}
+
+func (d *Data) SET(key string, value string) {
+	d.mData[key] = value
 }
