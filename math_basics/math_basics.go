10a2676b9c92b94023ad3d2bb1e1e0fdc5397173
0x4E43 <nimaic.dev@gmail.com>
2023-12-22 19:40:20 +0530

    number of digits in a number

--- math_basics/math_basics.go
+++ math_basics/math_basics.go
@@ -1,6 +1,9 @@
 package mathbasics
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func CountNumOfDigits(num int) {
 	digits := 0
@@ -8,4 +11,10 @@ func CountNumOfDigits(num int) {
 		digits++
 	}
 	fmt.Println("Number of digits in : ", num, " : ", digits)
+
+	//method 2 -> Convert into string and calculate String length
+
+	len := len(strconv.Itoa(num))
+	fmt.Println("Number of digits in(using string method) : ", num, " : ", len)
+
 }
