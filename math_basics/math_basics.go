ba9fb06525d639fc1a52aabda82f7cf5bc117521
0x4E43 <nimaic.dev@gmail.com>
2023-12-22 19:55:30 +0530

    Coding Ninja | Count Digits

--- math_basics/math_basics.go
+++ math_basics/math_basics.go
@@ -16,5 +16,20 @@ func CountNumOfDigits(num int) {
 
 	len := len(strconv.Itoa(num))
 	fmt.Println("Number of digits in(using string method) : ", num, " : ", len)
+	countNumberOfEvenlyDividingDigits(num)
+}
 
+// PROBLEM LINK: https://www.codingninjas.com/studio/problems/count-digits_8416387
+func countNumberOfEvenlyDividingDigits(num int) {
+	if num != 0 {
+		count := 0
+		for i := num; i != 0; i = i / 10 {
+			div := i % 10
+			if div != 0 && num%div == 0 {
+				count++
+				fmt.Println("DIGIT: ", div)
+			}
+		}
+		fmt.Println("Num of evenly deviding digits: ", count)
+	}
 }
