6e04c8ba94097628fea228b2ec3dbe61bdb95354
0x4E43 <nimaic.dev@gmail.com>
2023-12-27 19:41:35 +0530

    Palindrome Problem

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -1,7 +1,9 @@
 package leetcode
 
+import "strconv"
+
 // Problem Link : https://leetcode.com/problems/two-sum/submissions/
-func twoSum(nums []int, target int) []int {
+func TwoSum(nums []int, target int) []int {
 	var ret = make([]int, 2)
 	for i := 0; i < len(nums); i++ {
 		for j := 0; j < i; j++ {
@@ -13,4 +15,22 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return ret
+}
+
+// Problem Link: https://leetcode.com/problems/palindrome-number/
+func IsPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	if str[0] != str[len(str)-1] {
+		return false
+	}
+	runes := []rune(str)
+	// Reverse the order of the runes
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	if str == string(runes) {
+		return true
+	}
+	return false
 }
