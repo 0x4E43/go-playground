5dca49ac4f7cba334d6108d97abd3772d45df613
0x4E43 <nimaic.dev@gmail.com>
2023-12-28 23:52:47 +0530

    Printing Type of variable

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ package main
 
 import (
 	"fmt"
-
-	mathbasics "github.com/0x4E43/go-playground/math_basics"
-	"github.com/0x4E43/go-playground/sorting"
 )
 
 func main() {
 
-	mathbasics.CountNumOfDigits(12345)
-	// arr := [6]int{13, 46, 24, 52, 20, 9}
-	arr := [7]int{2, 13, 4, 1, 3, 6, 28}
-	fmt.Println("WITHOUT SORT: ", arr)
-	// sorting.SelectionSort(arr)
-	sorting.BubbleSort(arr)
+	// mathbasics.CountNumOfDigits(12345)
+	// // arr := [6]int{13, 46, 24, 52, 20, 9}
+	// arr := [7]int{2, 13, 4, 1, 3, 6, 28}
+	// fmt.Println("WITHOUT SORT: ", arr)
+	// // sorting.SelectionSort(arr)
+	// sorting.BubbleSort(arr)
+
+	var name string = "nimai"
+	fmt.Printf("Type: %T \n", name)
 }
