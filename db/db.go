b5165c86a63146c2940625945e82b3ec4258d1a8
0x4E43 <nimaic.dev@gmail.com>
2024-01-10 21:15:44 +0530

    Use of env

--- db/db.go
+++ db/db.go
@@ -3,12 +3,22 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+}
 func Db() {
+
+	fmt.Println(os.Getenv("DB_NAME"))
 	user := "nimai" //need to be taken from .env
 	passw := ""
 	db, err := sql.Open("mysql", user+":"+passw+"@tcp(10.10.10.11:3306)/report_generator")
