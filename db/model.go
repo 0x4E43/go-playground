c98c38b0d600dffc06ff1462dabc3f07f46f72ac
0x4E43 <nimaic.dev@gmail.com>
2024-01-12 21:29:14 +0530

    Added Models for DB

---
+++ db/model.go
@@ -0,0 +1,12 @@
+package db
+
+type NodeMaster struct {
+	Id              int16
+	DeviceId        int16
+	ControllerId    int16
+	IEEAddress      string
+	Intensity       int16
+	Occupancy       int16
+	IsIntensitySent int8
+	IsOccupancySent int8
+}
